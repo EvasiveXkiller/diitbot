e94eb4d59b68ba62d2227baa78b90d10f68721ba
EvasiveXkiller <58895502+EvasiveXkiller@users.noreply.github.com>
2021-01-22 20:58:00 +0800

    added new command and quick offline

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const prefix = '$';
+const prefix = "$";
 
-client.once('ready', () => {
-console.log('The International of DIIT Congress is online.');
+client.once("ready", () => {
+	console.log("The International of DIIT Congress is online.");
 });
 
-client.on('message', message => {
-if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-const args = message.content.slice(prefix.length).split(/ +/);
-const command = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-if(command === 'hello'){
-    message.channel.send('hey!');
-}
-
+	if (command === "hello") {
+		message.channel.send("hey!");
+    }
+    else if(command == "destroy"){
+        message.channel.send("bot going offline")
+        client.destroy();
+    }
 });
-
+client.login('ODAyMTE4MTAwMjEyMTIxNjAw.YAqksQ.QkVbWD8IDVzJijJBH0SdZYlqAHs')

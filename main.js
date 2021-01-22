4355189e0a5ea4e91e810aee33b2c99b5298fa84
TongKenSoon <77837423+TongKenSoon@users.noreply.github.com>
2021-01-22 20:37:04 +0800

    intial

---
+++ main.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '$';
+
+client.once('ready', () => {
+console.log('The International of DIIT Congress is online.');
+});
+
+client.on('message', message => {
+if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+const args = message.content.slice(prefix.length).split(/ +/);
+const command = args.shift().toLowerCase();
+
+if(command === 'hello'){
+    message.channel.send('Hi!');
+}
+});
+
+client.login('ODAyMTE4MTAwMjEyMTIxNjAw.YAqksQ.QkVbWD8IDVzJijJBH0SdZYlqAHs');

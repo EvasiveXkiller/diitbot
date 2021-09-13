319def266ebd39c502afff28e5f05ed7ae5e8be0
EvasiveXkiller <evasivexkiller@gmail.com>
2021-09-13 21:07:25 +0800

    deprecate useless files

--- stale/dbtest.js
+++
@@ -1,8 +0,0 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync("dbtest.json");
-const db = low(adapter);
-
-
-db.defaults({ posts: [], user: {}, count: 0 })
-  .write()
